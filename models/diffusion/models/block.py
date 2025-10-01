b61cd6c72888b22ecda4393af5a4447d65e836f9
Daechul Jung <jungdaechul@berkeley.edu>
2025-09-30 21:52:38 -0500

    unet construction

--- models/diffusion/models/block.py
+++ models/diffusion/models/block.py
@@ -9,10 +9,13 @@ class SiLU(nn.Module):
     
 
 class ResBlock(nn.Module):
+    """
+    ResNet block for up and down sampling 
+    """
     def __init__(self, input_channel, output_channel, time_dim, cond_dim = 0):
         super().__init__()
         ### Different convolution block from 2
-        self.norm1 = nn.GroupNorm(input_channel, input_channel)
+        self.norm1 = nn.GroupNorm(input_channel, input_channel) ## Applies group normalization over a mini-batch, similar to batch norm
         self.activ1 = SiLU()
         self.conv1 = nn.Conv2d(in_channels=input_channel, output_channel=output_channel, kernel_size=3, padding= 1) ## Based on the kernel size sum of the h + w would not be changed but their values are slightly changed 
 
@@ -21,6 +24,7 @@ class ResBlock(nn.Module):
         self.conv2 = nn.Conv2d(output_channel, output_channel, 3, padding= 1)
 
         self.emb = nn.Sequential(
+            ### Activation first and projection
             nn.SiLU(),
             nn.Linear(time_dim + cond_dim, output_channel)
         )
@@ -28,19 +32,22 @@ class ResBlock(nn.Module):
         self.short = nn.Conv2d(input_channel, output_channel, 1) if input_channel != output_channel else nn.Identity()
 
     def forward(self, x, time_emb, cond_emb = None):
-        h = self.conv1(self.activ1(self.norm1(x)))
-        emb = time_emb if cond_emb is None else torch.cat([time_emb, cond_emb], dim = -1)
-        h = h + self.emb(emb)[:, :, None, None]
-        h = self.conv2(self.activ2(self.norm2))
+        h = self.conv1(self.activ1(self.norm1(x))) ## normalize -> activation -> convolution
+        emb = time_emb if cond_emb is None else torch.cat([time_emb, cond_emb], dim = -1) ## Concatenate time and condition embedding 
+        h = h + self.emb(emb)[:, :, None, None] ## embedding and largening the dimensions and adding with the normalized input
+        h = self.conv2(self.activ2(self.norm2)) ## Same process as first h
         return h + self.short(x)
     
 
 class SelfAttention2d(nn.Module):
     def __init__(self, channels, num_heads):
+        """
+        Attention model for 2d input
+        """
         super().__init__()
         self.num_heads = num_heads
-        self.q = nn.Conv2d(channels, channels, 1)
-        self.v = nn.Conv2d(channels, channels, 1)
+        self.q = nn.Conv2d(channels, channels, kernel_size=1)
+        self.v = nn.Conv2d(channels, channels, 1) ### Even though (c, c, 1) shape of convolution, with (b, c, h, w), it returns (b, c, h, w) and does not impact on batch and channel. only impact on h, w 
         self.k = nn.Conv2d(channels, channels, 1)
 
         self.proj = nn.Conv2d(channels, channels, 1)
@@ -49,7 +56,7 @@ class SelfAttention2d(nn.Module):
         b, c, h, w = x.shape
 
         q = self.q(x).view(b, self.num_heads, c // self.num_heads, h * w) ### convert (b, c, h, w) -> (b, c, h, w) -> (b, num_heads, channel // num_heads, h * w)
-        v = self.v(x).view(b, self.num_heads, c // self.num_heads, h * w)
+        v = self.v(x).view(b, self.num_heads, c // self.num_heads, h * w) 
         k = self.k(x).view(b, self.num_heads, c // self.num_heads, h * 2)
 
         attn = torch.softmax((q.transpose(-2, -1))/ math.sqrt(c // self.num_heads), dim= -1)
@@ -58,8 +65,40 @@ class SelfAttention2d(nn.Module):
         return self.proj(out) + x
     
 class Down(nn.Module):
+    """
+    Down sampling for UNet Denoiser
+    """
     def __init__(self, input_channel, output_channel, time_dim, cond_dim = 0, use_attn = False):
         super().__init__()
         self.res1 = ResBlock(input_channel=input_channel, output_channel=output_channel, time_dim=time_dim, cond_dim=cond_dim)
         self.res2 = ResBlock(input_channel=output_channel, output_channel=output_channel, time_dim=time_dim, cond_dim=cond_dim)
-        
+        self.attn = SelfAttention2d(output_channel) if use_attn else nn.Identity()
+        self.pool = nn.Conv2d(output_channel, output_channel, kernel_size=3, stride = 2, padding=1)
+
+    def forward(self, x, skip, time_emb, cond_emb = None):
+        x = self.res1(x, time_emb, cond_emb) 
+        x = self.res2(x, time_emb, cond_emb)
+        x = self.attn(x)
+        skip = x
+        x = self.pool(x)
+        return x, skip
+    
+class Up(nn.Module):
+    """
+    Up sampling for UNet Denoiser
+    """
+    def __init__(self, input_channel, output_channel, time_dim, cond_dim = 0, use_attn = False):
+        super().__init__()
+        self.res1 = ResBlock(input_channel + output_channel, output_channel, time_dim, cond_dim)
+        self.res2 = ResBlock(output_channel, output_channel, time_dim, cond_dim)
+        self.attn = SelfAttention2d(output_channel) if use_attn else nn.Identity()
+        self.upsample = nn.ConvTranspose2d(output_channel, output_channel, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x, skip, time_emb, cond_emb = None):
+        x = torch.cat([x, skip], dim = 1)
+        x = self.res1(x, time_emb, cond_emb)
+        x = self.res2(x, time_emb, cond_emb)
+        x = self.attn(x)
+        self.upsample(x)
+
+        return x
---
+++ models/diffusion/models/unet.py
@@ -0,0 +1,57 @@
+from typing import List, Optional
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from models.diffusion.utils.embedding import *
+from models.diffusion.models.block import Down, Up, ResBlock, SelfAttention2d, SiLU
+
+class UNetDenoiser(nn.Module):
+    def __init__(self, in_channels, base, time_dim, cond_dim, attn_res: List[int]):
+        super().__init__()
+        self.time_mlp = nn.Sequential(nn.Linear(time_dim, time_dim), nn.SiLU(), nn.Linear(time_dim, time_dim))
+        self.cond_proj = nn.Sequential(nn.Linear(cond_dim, cond_dim), nn.SiLU()) if cond_dim > 0 else None
+
+        self.in_conv = nn.Conv2d(in_channels, base, kernel_size=3, padding= 1)
+
+        self.down1 = Down(base, base, time_dim, cond_dim, use_attn=(32 in attn_res))
+        self.down2 = Down(base, base*2, time_dim=time_dim, cond_dim=cond_dim, use_attn=(16 in attn_res))
+        self.down3 = Down(base*2, base*2, time_dim=time_dim, cond_dim=cond_dim, use_attn=(8 in attn_res))
+
+        self.mid1 = ResBlock(base*2, base*2, time_dim=time_dim, cond_dim=cond_dim)
+        self.mid_attn = SelfAttention2d(base*2)
+        self.mid2 = ResBlock(base*2, base*2, time_dim, cond_dim)
+
+        self.up3 = Up(base*2, base*2, time_dim, cond_dim)
+        self.up2 = Up(base*2, base, time_dim, cond_dim)
+        self.up1 = Up(base, base, time_dim, cond_dim)
+
+        self.out_norm = nn.GroupNorm(32, base)
+        self.out_act = SiLU()
+        self.out_conv = nn.Conv2d(base, in_channels, kernel_size=3, padding = 1)
+
+    def forward(self, x, t: torch.LongTensor, cond : Optional[torch.Tensor] = None):
+        time_emb = self.time_mlp(timestep_embedding(t, self.time_mlp[0].in_features))
+        cond_emb = self.cond_proj(cond) if (self.cond_proj is not None and cond is not None) else None 
+
+        x = self.in_conv(x)
+
+        x, skip1 = self.down1(x, time_emb, cond_emb)
+        x, skip2 = self.down2(x, time_emb, cond_emb)
+        x, skip3 = self.down3(x, time_emb, cond_emb)
+
+        x = self.mid1(x, time_emb, cond_emb)
+        x = self.mid_attn(x)
+        x = self.mid2(x, time_emb, cond_emb)
+
+        x = F.interpolate(x, scale_factor=2, mode='nearest')
+        x = self.up3(x, skip3, time_emb, cond_emb)
+        x = self.up2(x, skip2, time_emb, cond_emb)
+        x = self.up1(x, skip1, time_emb, cond_emb)
+
+        x = self.out_conv(self.out_act(self.out_norm(x)))
+
+        return x ## this predict epsilon prime
+
